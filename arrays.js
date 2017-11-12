c655257842f3ab0ad209d60ac95df7ca186fc78e
Monica Marziano <mmarziano1@gmail.com>
2017-11-12 13:32:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,8 @@
 var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray (array, element) {]
+  var array =  ["red", "white", "blue"];
+  var element = "green";
+  
+  
+}
